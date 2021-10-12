bc0af11615444d37930d4425b1251e685c359e56
kimziont <ziontkim0510@gmail.com>
2021-10-13 02:43:28 +0900

    Valid_Palindrome first trial

---
+++ python_algorithm_interview/Valid_Palindrome/trial_1.py
@@ -0,0 +1,12 @@
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub('[^a-zA-Z0-9]', '', s)
+        s = s.lower()
+        return s == s[::-1]
+
+
+test = Solution()
+assert test.isPalindrome("A man, a plan, a canal: Panama")
